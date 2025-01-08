d56cd67b31d2d3d4d6a1186c04637523cdd5115a
puny80 <puny4me@citizen.seoul.kr>
2025-01-08 13:53:24 +0900

    이모지추가1

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo. 🎉🎄
+        🥳 App successfully hosted. Try creating a new todo. 🎉🎄🎫
         <br />
         <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
           Review next steps of this tutorial.

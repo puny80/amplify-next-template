381d94f35d4594eb935131888d434ae3c2ab655e
puny80 <to_22001@yahoo.co.kr>
2025-01-08 14:17:55 +0900

    사인된 커밋2

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo. 🎉🎄🎫🎪
+        🥳 App successfully hosted. Try creating a new todo. 🎉🎄🎫🎪✨
         <br />
         <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
           Review next steps of this tutorial.
